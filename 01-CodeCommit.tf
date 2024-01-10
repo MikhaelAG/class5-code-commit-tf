ba8a6a2ea4b64e00b33914602757c40f8d956da7
MikhaelAG <130944619+MikhaelAG@users.noreply.github.com>
2024-01-09 20:49:04 -0600

    Update 01-CodeCommit.tf

--- 01-CodeCommit.tf
+++ 01-CodeCommit.tf
@@ -3,4 +3,16 @@ resource "aws_codecommit_repository" "Repos" {
     for_each = toset(var.repo_names)
     repository_name = each.value
     description = "GitHub repo for class 5"
+
+      # ignore tag added by CodeGuru service upon association
+  lifecycle {
+    ignore_changes = [
+      tags["codeguru-reviewer"]
+    ]
+  }
+}
+
+# Grab KMS keys from repos
+resource "aws_kms_key" "kms_keys" {
+    for_each = toset(var.repo_names)
 }
