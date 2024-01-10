428c6ed421895178de8d58386e5423fad4117a42
MikhaelAG <130944619+MikhaelAG@users.noreply.github.com>
2024-01-09 20:48:28 -0600

    Update and rename 03-Outputs.tf to 04-Outputs.tf

--- 04-Outputs.tf
+++ 04-Outputs.tf
@@ -7,3 +7,9 @@ output "Repo"{
         for key, value in aws_codecommit_repository.Repos: key => "Clone_url_https: ${value.clone_url_http}"
     }
 }
+
+output "Repo_kms_key_ids"{
+    value = {
+        for key, value in aws_kms_key.kms_keys: key=> value.key_id
+    }
+}
